28a92d2e9628ce2ce32181fb74eed6e6054bdad2
swarup-1 <swarupkadoli9@gmail.com>
2023-02-14 19:40:26 +0530

    carousel

---
+++ src/Components/carousel2.data.js
@@ -0,0 +1,145 @@
+export const data=[
+    {
+        "image2": "https://www.rentomojo.com/bangalore/electronics/rent-oneplus-7-pro/91631",
+        "image": "https://p.rmjo.in/productSquare/mxzy0xf9-500x500.jpg",
+        "title": "OnePlus 7 Pro (Mirror Gray)",
+        "price": "1669",
+        "days": "3 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "id": 4
+      },      {
+        "id": 3000018,
+        "image": "https://p.rmjo.in/productSquare/e2nc1yzg-500x500.jpg",
+        "title": "LED TV - 40",
+        "price": 1269,
+        "stock": "available",
+        "category": "telivision",
+        "days": "3 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg"
+      },      {
+        "id": 3000012,
+        "image": "https://p.rmjo.in/productSquare/z227w1xz-500x500.jpg",
+        "title": "Semi Automatic Washing Machine",
+        "price": 479,
+        "stock": "available",
+        "category": "washingmachine",
+        "days": "2 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg"
+      },    
+    {
+        "id": 300005,
+        "image": "https://p.rmjo.in/productSquare/x0to9y4x-500x500.jpg",
+        "title": "Mini Fridge",
+        "price": 449,
+        "stock": "available",
+        "category": "fridge",
+        "days": "3 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg"
+      },      
+    {  "image": "https://p.rmjo.in/productSquare/sk1pofns-500x500.jpg",
+      "title": "Aurora Wooden Queen Bed (6x5)",
+      "price": 539,
+      "stock": "available",
+      "category": "bedroom",
+      "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+      "days": "3 days",
+      "id": 124
+    },   {
+        "image": "https://p.rmjo.in/productSquare/75pzthwi-500x500.jpg",
+        "title": "Napster Metal Single Bed (6x3)",
+        "price": 249,
+        "stock": "available",
+        "category": "bedroom",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 7
+      },
+      {
+        "image": "https://p.rmjo.in/productSquare/1eoam6o5-500x500.jpg",
+        "title": "Poise Study Table",
+        "price": 229,
+        "stock": "available",
+        "category": "wfh",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 117
+      },{
+        "image": "https://p.rmjo.in/productSquare/s9wlr8o3-500x500.jpg",
+        "title": "Poise Wooden King Bed (6x6)",
+        "price": 669,
+        "stock": "Out of stock",
+        "category": "bedroom",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 9
+      },      {
+        "image": "https://p.rmjo.in/productSquare/s31yoe0u-500x500.jpg",
+        "title": "Stowy 2-Door Wardrobe",
+        "price": 409,
+        "stock": "available",
+        "category": "bedroom",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 18
+      },      {
+        "id": 1,
+        "image": "https://p.rmjo.in/productSquare/em6h6xgy-500x500.jpg",
+        "title": "Stuart Study Table",
+        "price": "149",
+        "o_price": "72"
+      },      {
+        "image": "https://p.rmjo.in/productSquare/akv269ws-500x500.jpg",
+        "title": "Miller Office Chair",
+        "price": 149,
+        "stock": "available",
+        "category": "living",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 152
+      },      {
+        "image": "https://p.rmjo.in/productSquare/yunuytfl-500x500.jpg",
+        "title": "Rowling Bookshelf Small",
+        "price": 109,
+        "stock": "available",
+        "category": "bedroom",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 23
+      },   {
+        "id": 300006,
+        "image": "https://p.rmjo.in/productSquare/ry27prsa-500x500.jpg",
+        "title": "Single Door Fridge (210 Litre)",
+        "price": 809,
+        "stock": "available",
+        "category": "fridge",
+        "days": "3 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg"
+      },      {
+        "id": 3000013,
+        "image": "https://p.rmjo.in/productSquare/zufkl87m-500x500.jpg",
+        "title": "Microwave Solo 20L",
+        "price": 299,
+        "stock": "available",
+        "category": "microwaveandinduction",
+        "days": "3 days",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg"
+      }, {
+        "image": "https://p.rmjo.in/productSquare/durxsu1c-500x500.jpg",
+        "title": "Morris Office Chair",
+        "price": 199,
+        "stock": "available",
+        "category": "living",
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "id": 151
+      } , {
+        "image": "https://p.rmjo.in/productSquare/4mb23xd0-500x500.jpg",
+        "title": "Atticus Wooden Single Platform Bed (6x3)",
+        "price": 339,
+        "delivery": "https://www.rentomojo.com/public/images/fast-delivery/fast-delivery.svg",
+        "days": "3 days",
+        "stock": "available",
+        "category": "bedroom",
+        "id": 11
+      }
+]
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,17 +3,27 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { NavLink } from "react-router-dom"
-import { Grid, Box, Text, Image} from "@chakra-ui/react"
+import { Grid, Box, Text, Image, Button, Icon} from "@chakra-ui/react"
+import {BsArrowRightCircle,BsArrowLeftCircle} from "react-icons/bs";
 import Package from "../Assets/Package.png"
 import furniture from "../Assets/furniture.png"
 import applience from "../Assets/applience.png"
 import Electronics from "../Assets/Electronics.png"
 import fitness from "../Assets/fitness.png"
 import wfh from "../Assets/wfh.png"
+import data from "../Components/carousel2.data"
 
 const Home = () => {
+  const prev=()=>{
+    let prevbox=document.querySelector(`.cards`);
+    prevbox.scrollLeft-=400
+  }
+  const next=()=>{
+    let nextbox=document.querySelector(`.cards`);
+    nextbox.scrollLeft+=400
+  }
   return (
-    <div>
+    <div style={{backgroundColor:"white"}}>
         <Box mt="2" width="80%" align="center" margin="auto">
           <Slider infinite={true} dots={true} lazyLoad={true} autoplay={true} slidesToShow={1} slidesToScroll={1} autoplaySpeed={3000} >
               <div>
@@ -32,37 +42,48 @@ const Home = () => {
             <Text  textDecoration="none" fontSize="xs">Packages</Text>
           </Box>
         </NavLink>
-        <NavLink  to={"./packages"}>
+        <NavLink  to={"./furniture"}>
           <Box py="10px" px={{base:"20px", md:"40px",xl:"50px"}} border={"1px solid #ebf0f4 "} transition="ease-in-out" transitionDuration={"400ms"} rounded={"md"} _hover={{boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"}}>
             <Image boxSize={"50px"} m="auto" src={furniture} ></Image>
             <Text  textDecoration="none" fontSize="xs">Furniture</Text>
           </Box>
         </NavLink>
-        <NavLink  to={"./packages"}>
+        <NavLink  to={"./appliances"}>
           <Box py="10px" px={{base:"20px", md:"40px",xl:"50px"}} border={"1px solid #ebf0f4 "} transition="ease-in-out" transitionDuration={"400ms"} rounded={"md"} _hover={{boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"}}>
             <Image boxSize={"50px"} m="auto" src={applience} ></Image>
             <Text  textDecoration="none" fontSize="xs">Appliences</Text>
           </Box>
         </NavLink>
-        <NavLink  to={"./packages"}>
+        <NavLink  to={"./electronics"}>
           <Box py="10px" px={{base:"20px", md:"40px",xl:"50px"}} border={"1px solid #ebf0f4 "} transition="ease-in-out" transitionDuration={"400ms"} rounded={"md"} _hover={{boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"}}>
             <Image boxSize={"50px"} m="auto" src={Electronics} ></Image>
             <Text  textDecoration="none" fontSize="xs">Electronics</Text>
           </Box>
         </NavLink>
-        <NavLink  to={"./packages"}>
+        <NavLink  to={"./fitness"}>
           <Box py="10px" px={{base:"20px", md:"40px",xl:"50px"}} border={"1px solid #ebf0f4 "} transition="ease-in-out" transitionDuration={"400ms"} rounded={"md"} _hover={{boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"}}>
             <Image boxSize={"50px"} m="auto" src={fitness} ></Image>
             <Text  textDecoration="none" fontSize="xs">Fitness</Text>
           </Box>
         </NavLink>
-        <NavLink  to={"./packages"}>
+        <NavLink  to={"./wfh"}>
           <Box py="10px" px={{base:"20px", md:"40px",xl:"50px"}} border={"1px solid #ebf0f4 "} transition="ease-in-out" transitionDuration={"400ms"} rounded={"md"} _hover={{boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"}}>
             <Image boxSize={"50px"} m="auto" src={wfh} ></Image>
             <Text  textDecoration="none" fontSize="xs">WFH Essentials</Text>
           </Box>
         </NavLink>
       </Grid>
+      <Box position="relative" overflow="hidden" padding="26px" backgroundColor="#f5f7fa" py="60px" >
+        <Text width={"130px"} textAlign={"left"} color="black" ml="2%">You'll love to take these Home!!</Text>
+        <hr style={{color:"red", backgroundColor:"red", width:"5%", padding:"1px", marginTop:"10px", marginLeft:"2%" }}/>
+        <Box padding=" 0px" display=" flex" gap="10px" flexDirection="row-reverse" >
+        <Button onClick={next}><BsArrowRightCircle/></Button>
+        <Button onClick={prev}><BsArrowLeftCircle/></Button>
+        </Box>
+        <Box className='cards' display="flex" gap="20px" padding="0px 10px" overflow="hidden" scroll-behavior="smooth" id="productsss" >
+            {/* {data.map(el=><Cards image={el.image} key={Date.now()+el.id} title={el.title} price={el.price} prod={el}></Cards>)} */}
+        </Box>
+      </Box>
       {/* <FurnitureSlider></FurnitureSlider>
       <SimpleThreeColumns ></SimpleThreeColumns>
       <FurnitureSlider2></FurnitureSlider2> */}
